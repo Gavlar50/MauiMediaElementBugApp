159bdfd5dfa475b9182aed81dc2ba99669f1b060
Gavlar50 <Gavlar50@users.noreply.github.com>
2026-03-27 12:42:24 +0000

    Update MainPage.xaml.cs
    added some useful code comments

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -9,6 +9,8 @@
         {
             InitializeComponent();
             _playbackSpeed = 1.0;
+
+            //TODO replace this with the absolute path to your video location - refer to the repo docs for sample videos to download for each use case
             MyPlayer.Source = "d:\\dev\\mauimediaelementbugapp\\testmovie.mp4";
         }
 
@@ -21,6 +23,8 @@
             MyPlayer.Play();
         }
 
+        // this simulates something like a cycling app where the playback speed increases relative to the original
+        // as the user pedals faster, but the stuttering issue occurs when the video includes an audio track
         private void Timer_Tick(object sender, EventArgs e)
         {
             // note how every time this fires, a movie with sound will briefly stutter while the speed change occurs
